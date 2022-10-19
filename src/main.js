8c689e521c27fff8a007c2e5a7417a19665c956e
MolderG <moldergs@gmail.com>
2022-10-19 11:32:54 -0300

    first class - introduction to JS DOM

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-import "./css/index.css"
+import './css/index.css'
+
+const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
+const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
+const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img')
+
+const setCardType = typeCard => {
+  const colors = {
+    visa: ['#2D57F2', '#436D99'],
+    mastercard: ['#DF6F29', '#C69347'],
+    elo: ['#979943', '#E32DF2'],
+    default: ['black', 'gray']
+  }
+  ccBgColor01.setAttribute('fill', colors[typeCard][0])
+  ccBgColor02.setAttribute('fill', colors[typeCard][1])
+  ccLogo.setAttribute('src', `cc-${typeCard}.svg`)
+}
+
+setCardType('visa')
+
+globalThis.setCardType = setCardType
