73ef25e56ac9de355700ccd3824d9f6c1b52c660
MolderG <moldergs@gmail.com>
2022-10-22 20:29:24 -0300

    second class

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './css/index.css'
+import IMask from 'imask'
 
 const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
 const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
@@ -8,7 +9,6 @@ const setCardType = typeCard => {
   const colors = {
     visa: ['#2D57F2', '#436D99'],
     mastercard: ['#DF6F29', '#C69347'],
-    elo: ['#979943', '#E32DF2'],
     default: ['black', 'gray']
   }
   ccBgColor01.setAttribute('fill', colors[typeCard][0])
@@ -16,6 +16,59 @@ const setCardType = typeCard => {
   ccLogo.setAttribute('src', `cc-${typeCard}.svg`)
 }
 
-setCardType('visa')
-
 globalThis.setCardType = setCardType
+
+const securityCode = document.querySelector('#security-code')
+const securityCodePattern = {
+  mask: '000'
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector('#expiration-date')
+const expirationDatePattern = {
+  mask: 'MM{/}YY',
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 7).slice(2)
+    }
+  }
+}
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector('#card-number')
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: '0000 0000 0000 0000',
+      regex: /^4\d{0,15}/,
+      cardType: 'visa'
+    },
+    {
+      mask: '0000 0000 0000 0000',
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2{0,2})\d{0,12}/,
+      cardType: 'mastercard'
+    },
+    {
+      mask: '0000 0000 0000 0000',
+      cardType: 'default'
+    }
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, '')
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+
+    return foundMask
+  }
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
